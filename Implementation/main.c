a7275a739aece8c99a26bc337ed10e70a309235e
99003607 <78539475+99003607@users.noreply.github.com>
2021-02-05 18:38:55 +0530

    Update main.c

--- Implementation/main.c
+++ Implementation/main.c
@@ -1,5 +1,37 @@
 #include<stdio.h>
-void main()
+int main()
 {
-	printf("this is main file");
+	int choice;
+	printf("--------------Welcome to the calculator------------");
+	printf("\n---Calculator Menu---\n");
+	first:printf("/nList of operations\n 1)Basic arthmetic calculations   2)Boolean operations  3)Trignometry  \n 4)Base conversion  5)Exponential  6)Qudratic \n 7)Unit convertions  8)Reverse conversion  9)Statistical conversion  /n 10)EXIT");
+	printf("\nEnter your choice:");
+	scanf("%d",&choice);
+	switch(choice)
+	{
+		case 1: basic();
+			break;
+		case 2: boolean();
+			break;
+		case 3: trignometry();
+			break;
+		case 4: baseconversion();
+			break;
+		case 5: exponential();
+			break;
+		case 6: quadratic();
+			break;
+		case 7: unitconversion();
+			break;
+		case 8: reverseconversion();
+			break;
+		case 9: statitics();
+			break;
+		case 10: exit(); 
+			
+		default: printf("INVALID CHOICE!!!!!!\nPlease renter your choice");
+			goto first;
+	}
+	goto first;
+	return(0);
 }
