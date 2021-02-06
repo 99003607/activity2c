6a7f49112e6d3cc71725d14c6f1bedff2434ec21
99003607 <78539475+99003607@users.noreply.github.com>
2021-02-06 17:20:39 +0530

    Update test_unitconver.h

--- Implementation/test/test_unitconver.h
+++ Implementation/test/test_unitconver.h
@@ -3,4 +3,10 @@
 
 int test_main(void);
 
+* Prototypes for all the test functions */
+void test_temperature(void);
+void test_time(void);
+void test_length(void);
+void test_weight(void);
+
 #endif 
