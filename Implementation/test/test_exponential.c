59245f5a9f3e3a7dd3d8aa6d0efbf0d4a981151f
99003611 <78539830+99003611@users.noreply.github.com>
2021-02-06 15:30:09 +0530

    added(text_expo)

---
+++ Implementation/test/test_exponential.c
@@ -0,0 +1,46 @@
+#include <unity.h>
+//#include <calculator_operations.h>
+#include<exponential.h>
+
+/* Modify these two lines according to the project */
+#include <calculator_operations.h>
+#define PROJECT_NAME    "Calculator"
+
+/* Prototypes for all the test functions */
+void test_expo(void);
+void test_power(void);
+
+
+/* Required by the unity test framework */
+void setUp(){}
+/* Required by the unity test framework */
+void tearDown(){}
+
+/* Start of the application test */
+int main()
+{
+/* Initiate the Unity Test Framework */
+  UNITY_BEGIN();
+
+/* Run Test functions */
+  RUN_TEST(test_expo);
+  RUN_TEST(test_power);
+
+  /* Close the Unity Test Framework */
+  return UNITY_END();
+}
+
+/* Write all the test functions */ 
+void test_expo(void) {
+  TEST_ASSERT_EQUAL(1.0, expo(10,0));
+  TEST_ASSERT_EQUAL(2.718282, expo(10,1));
+  TEST_ASSERT_EQUAL(0.367879, expo(10,-1)); 
+  TEST_ASSERT_EQUAL(5.017, expo(10,3));
+   
+}
+void test_power(void) {
+    TEST_ASSERT_EQUAL(81.0, power(3,4));
+    TEST_ASSERT_EQUAL(387420480.0, power(9,9));
+    TEST_ASSERT_EQUAL(1, power(5,0));
+    TEST_ASSERT_EQUAL(0.2, power(5,-1));
+}
