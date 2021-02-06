e9e97e85e09ab958cf9222ac82501f32d3f3221f
99003611 <78539830+99003611@users.noreply.github.com>
2021-02-06 22:24:56 +0530

    Create test_exponential.h

---
+++ Implementation/test/test_exponential.h
@@ -0,0 +1,9 @@
+#ifndef __TEST_EXPONENTIAL_H__
+#define __TEST_EXPONENTIAL_H__
+void test_is_expo();
+
+// Prototypes for all the test functions 
+void test_expo(void);
+void test_power(void);
+
+#endif 
