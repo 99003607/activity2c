a3a0efb423b3e085b455f9dc76ea8f62369da88a
99003597 <78539729+99003597@users.noreply.github.com>
2021-02-06 18:02:46 +0530

    Update statistics.c

--- Implementation/test/statistics.c
+++ Implementation/test/statistics.c
@@ -27,48 +27,21 @@ int test_main()
   UNITY_BEGIN();
 
 /* Run Test functions */
-  RUN_TEST(test_temperature);
-  RUN_TEST(test_time);
-  RUN_TEST(test_length);
-  RUN_TEST(test_weight);
-
+  RUN_TEST(test_1);
+  
   /* Close the Unity Test Framework */
   return UNITY_END();
 }
 
 /* Write all the test functions */ 
-void test_temperature(void) {
+void test_1(void) {
 	//celcius to farenheit
-  TEST_ASSERT_EQUAL(50, temperature_func(10,1));
+  TEST_ASSERT_EQUAL(-12, statitics(2,-10,-14));
   //farenheit to celcius
-  TEST_ASSERT_EQUAL(15.55, temperature_func(60,2)); 
+  TEST_ASSERT_EQUAL(16, statitics(2,11,21)); 
    
 }
 
-void test_time(void) {
-	//seconds to minute
-  TEST_ASSERT_EQUAL(1.5, time_func(90,1));
-  
-  //minute to seconds
-  TEST_ASSERT_EQUAL(3900, time_func(65,2));
-}
-
-void test_length(void) {
-	//inch to cm
-  TEST_ASSERT_EQUAL(0, length_func(34,1));
-  
-  //cm to meter
-  TEST_ASSERT_EQUAL(14, length_func(1400,2));
-}
-
-void test_weight(void) {
-	//miligram to gram
-  TEST_ASSERT_EQUAL(8.240, weight_func(8240,1));
-  
-  //gram to kilogram
-  TEST_ASSERT_EQUAL(2.587, weight_func(2587,2));
-}
-/
 
 /* Required by the unity test framework */
 void setUp(){}
@@ -82,10 +55,8 @@ int test_main()
   UNITY_BEGIN();
 
 /* Run Test functions */
-  RUN_TEST(test_temperature);
-  RUN_TEST(test_time);
-  RUN_TEST(test_length);
-  RUN_TEST(test_weight);
+  RUN_TEST(test_1);
+ 
 
   /* Close the Unity Test Framework */
   return UNITY_END();
