791074a6917a7c0efab45872b97a556c8c492efe
99003613 <sayan.dey@ltts.com>
2021-02-06 17:08:57 +0530

    Update quadratic.c

--- Implementation/src/quadratic.c
+++ Implementation/src/quadratic.c
@@ -13,24 +13,25 @@ float getroots(float det,float x,float y,float z)
 {
 float root1,root2,real,img;
 //Defining the condition for real and equal roots
-if(det=0)
+if(det == 0)
  {  
         root1 = root2 = -y / (2 * x); // both roots are equal;  
         printf("\n Value of root1 = %.2f and Value of root2 = %.2f", root1, root2);  
  }  
 //Defining the condition for complex and different roots
-if(det<0) 
+else if(det < 0) 
     {
         real = -y / (2 * x);  
         img = sqrt(-det) / (2 * x);  
         printf("\n value of root1 = %.2f + %.2fi and value of root2 = %.2f - %.2fi ", real, img, real, img);  
     }  
 //Defining the codition for real and different roots
-else{
+else
+ {
     root1 = (-y + sqrt(det)) / (2 * x);  
     root2 = (-y + sqrt(det)) / (2 * x);  
     printf("\n Value of root1 = %.2f and value of root2 = %.2f", root1, root2); 
-    }
+ }
 }
 void quadratic()
 {
