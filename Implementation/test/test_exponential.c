bd8509af33e73808595466c86fe8fec6a3d3c249
99003611 <78539830+99003611@users.noreply.github.com>
2021-02-06 22:55:20 +0530

    Update test_exponential.c

--- Implementation/test/test_exponential.c
+++ Implementation/test/test_exponential.c
@@ -16,13 +16,13 @@ void test_expo(void) {
   TEST_ASSERT_EQUAL(1.0, expo(10,0));
   TEST_ASSERT_EQUAL(2.718282, expo(10,1));
   TEST_ASSERT_EQUAL(0.367879, expo(10,-1)); 
-  TEST_ASSERT_EQUAL(5.017, expo(10,3));
+  TEST_ASSERT_EQUAL(20, expo(10,3));
   TEST_ASSERT_EQUAL(13.04765, expo(10,2));
    
 }
 void test_power(void) {
     TEST_ASSERT_EQUAL(81.0, power(3,4));
-    TEST_ASSERT_EQUAL(387420480.0, power(9,9));
+    TEST_ASSERT_EQUAL(31381059584, power(9,9));
     TEST_ASSERT_EQUAL(1, power(5,0));
     TEST_ASSERT_EQUAL(0.2, power(5,-1));
     TEST_ASSERT_EQUAL(0.0256, power(4,-2));
