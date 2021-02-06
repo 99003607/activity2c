7b0a18ab12bb4929e6f6751027bdc3454d84365e
99003602 <78539860+99003602@users.noreply.github.com>
2021-02-06 22:29:55 +0530

    Create test_base.h

---
+++ Implementation/test/test_base.h
@@ -0,0 +1,12 @@
+#ifndef __TEST_BASE_H__
+#define __TEST_BASE_H__
+void test_base();
+
+// Prototypes for all the test functions 
+void test_temperature(void);
+void test_time(void);
+void test_length(void);
+void test_weight(void);
+
+
+#endif 
