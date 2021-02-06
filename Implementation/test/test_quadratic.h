929c49a157cd1a2783beb70e910c1492c7816736
99003611 <78539830+99003611@users.noreply.github.com>
2021-02-06 23:08:27 +0530

    Create test_quadratic.h

---
+++ Implementation/test/test_quadratic.h
@@ -0,0 +1,9 @@
+#ifndef __TEST_QUADRATIC_H__
+#define __TEST_QUADRATIC_H__
+void test_quadratic();
+
+// Prototypes for all the test functions 
+void test_Deter(void);
+void test_getroots(void);
+
+#endif 
