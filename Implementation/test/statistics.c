2c2500dbcd1840ff72df6b786caffc096563f278
99003611 <78539830+99003611@users.noreply.github.com>
2021-02-06 23:22:08 +0530

    Update statistics.c

--- Implementation/test/statistics.c
+++ Implementation/test/statistics.c
@@ -1,33 +1,16 @@
 #include <unity.h>
-#include "statistcs.h"
 #include<statitics.h>
+#include<test_statistcs.h>
 
-/* Modify these two lines according to the project */
-#include <calculator_operations.h>
-#define PROJECT_NAME    "Calculator"
 
-/
 
-/* Required by the unity test framework */
-void setUp(){}
-/* Required by the unity test framework */
-void tearDown(){}
 
-/* Start of the application test */
-int test_main()
+void test_statistics()
 {
-/* Initiate the Unity Test Framework */
-  UNITY_BEGIN();
-
-/* Run Test functions */
   RUN_TEST(test_1);
-
-
-  /* Close the Unity Test Framework */
-  return UNITY_END();
 }
 
-/* Write all the test functions */ 
+
 void test_1(void) {
 	//celcius to farenheit
   TEST_ASSERT_EQUAL(-12, statitics(-10,-14));
