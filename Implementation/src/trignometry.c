23431301b18920c02c87449f934e618d0e47d9ae
99003607 <sandesh.prabhu@ltts.com>
2021-02-06 15:42:05 +0530

    corrected trignometry

--- Implementation/src/trignometry.c
+++ Implementation/src/trignometry.c
@@ -8,7 +8,7 @@ void trignometry()
 	    printf("enter the number");
 	    scanf("%f",&angle);
 	    trig(angle);
-	    printf("enter 1 for continuation else 2");
+	    /*printf("enter 1 for continuation else 2");
 	    int k;
 	    scanf("%d",&k);
 	    if(k=1){
@@ -17,17 +17,17 @@ void trignometry()
 	    else{
 	        return 0;
 	
-	    }
-	}   
-	int trig(int angle)  
-	{  
-	    float degree, radian;  
-	    const float PI = 3.14159;  
+	    }*/
+}   
+int trig(int angle)  
+{  
+	float degree, radian;  
+	const float PI = 3.14159;  
 	  
-	    printf("Enter angle in degree\n");  
-	    scanf("%f",&degree);  
+	printf("Enter angle in degree\n");  
+	scanf("%f",&degree);  
 	  
-	    radian = degree * (PI / 180.0);  
+	radian = degree * (PI / 180.0);  
 	  
 	    printf("Sin(%f) = %f\n", degree, sin(radian));  
 	    printf("Cos(%f) = %f\n", degree, cos(radian));  
