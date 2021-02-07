891e125d9d3d225e379967de86f5a88fdec5baf9
99003607 <78539475+99003607@users.noreply.github.com>
2021-02-07 12:17:53 +0530

    Create test_logicaunittest.h

---
+++ Implementation/test/test_logicaunittest.h
@@ -0,0 +1,13 @@
+#ifndef __TEST_LOGICS_H__
+#define __TEST_LOGICS_H__
+void test_logics();
+
+// Prototypes for all the test functions 
+void test_or(void);
+void test_and(void);
+void test_nand(void);
+void test_nor(void);
+void test_xor(void);
+void test_xnor(void);
+
+#endif 
