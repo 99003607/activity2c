63e8d941c6bbab901adb7a6a85f0a29dcca859f8
99003607 <78539475+99003607@users.noreply.github.com>
2021-02-07 10:54:00 +0530

    Update test_trigno.h

--- Implementation/test/test_trigno.h
+++ Implementation/test/test_trigno.h
@@ -1,8 +1,11 @@
 #ifndef __TEST_TRIGNO_H__
 #define __TEST_TRIGNO_H__
-void test_trigno();
+//void test_trigno();
 
 // Prototypes for all the test functions 
 void test_is_trig(void);
+void test_sin(void);
+void test_cos(void);
+void test_tan(void);
 
 #endif 
