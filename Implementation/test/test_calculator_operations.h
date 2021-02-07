f5eee48e62d3094ecf85a8bbe82f281e1cfb80be
99003607 <78539475+99003607@users.noreply.github.com>
2021-02-07 10:09:37 +0530

    Create test_calculator_operations.h

---
+++ Implementation/test/test_calculator_operations.h
@@ -0,0 +1,11 @@
+#ifndef __TEST_CALC_H__
+#define __TEST_CALC_H__
+void test_calc();
+
+// Prototypes for all the test functions 
+void test_add(void);
+void test_subtract(void);
+void test_multiply(void);
+void test_divide(void);
+
+#endif 
