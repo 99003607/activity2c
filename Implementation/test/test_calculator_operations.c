4b7b4d63d2f997028093102db59a02977fd0e0a6
99003607 <78539475+99003607@users.noreply.github.com>
2021-02-07 10:09:49 +0530

    Update test_calculator_operations.c

--- Implementation/test/test_calculator_operations.c
+++ Implementation/test/test_calculator_operations.c
@@ -1,35 +1,18 @@
 #include "unity.h"
 #include <calculator_operations.h>
+#include<test_calculator_operations.h>
 
-/* Modify these two lines according to the project */
-#include <calculator_operations.h>
-#define PROJECT_NAME    "Calculator"
 
-/* Prototypes for all the test functions */
-void test_add(void);
-void test_subtract(void);
-void test_multiply(void);
-void test_divide(void);
-
-/* Required by the unity test framework */
-void setUp(){}
-/* Required by the unity test framework */
-void tearDown(){}
 
 /* Start of the application test */
-int main()
+void test_calc()
 {
-/* Initiate the Unity Test Framework */
-  UNITY_BEGIN();
 
 /* Run Test functions */
   RUN_TEST(test_add);
   RUN_TEST(test_subtract);
   RUN_TEST(test_multiply);
   RUN_TEST(test_divide);
-
-  /* Close the Unity Test Framework */
-  return UNITY_END();
 }
 
 /* Write all the test functions */ 
