1dcbf9d8473342d0ade367d666fc0432c439144e
99003607 <78539475+99003607@users.noreply.github.com>
2021-02-07 13:15:45 +0530

    Update test_logicaunit.c

--- Implementation/test/test_logicaunit.c
+++ Implementation/test/test_logicaunit.c
@@ -19,8 +19,6 @@ void test_logics()
 void test_and(void) {
 	//ANDing of two int values
   TEST_ASSERT_EQUAL(10,and(10,15));
-  //dummy fail
-  TEST_ASSERT_EQUAL(9,and(8, 15)); 
    
 }
 
@@ -32,29 +30,17 @@ void test_or(void) {
 void test_nand(void) {
 	//nand of two int values
   TEST_ASSERT_EQUAL(-11,nand(10, 15));
-  
-  //dummy fail
-  TEST_ASSERT_EQUAL(9,nand(8, 15));
 }
 
 void test_nor(void) {
 	//noring of two values
   TEST_ASSERT_EQUAL(-16,nor(10, 15));
-  
-  //dummy fail
-  TEST_ASSERT_EQUAL(9,nor(8, 15));
 }
 void test_xor(void) {
 	//xoring of two values
   TEST_ASSERT_EQUAL(5,xor(10, 15));
-  
-  //dummy fail
-  TEST_ASSERT_EQUAL(9,xor(8, 15));
 }
 void test_xnor(void) {
 	//xnoring of two values
   TEST_ASSERT_EQUAL(6,xnor(10, 15));
-  
-  //dummy fail
-  TEST_ASSERT_EQUAL(9,xnor(8, 15));
 }
