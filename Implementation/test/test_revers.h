f2f32b9aadf2d7e8baf6d90f9f01e06555540deb
99003607 <78539475+99003607@users.noreply.github.com>
2021-02-07 09:48:04 +0530

    Create test_revers.h

---
+++ Implementation/test/test_revers.h
@@ -0,0 +1,9 @@
+#ifndef __TEST_REVERS_H__
+#define __TEST_REVERS_H__
+void test_revers();
+
+// Prototypes for all the test functions 
+void convertoctalToDecimal(void);
+void convertbinaryToDecimal(void);
+
+#endif 
